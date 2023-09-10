8bec6bd04be2b39b16345febf66e18d2c45e96d0
Piotr Moryto <p.moryto@tidio.net>
2023-09-10 16:51:31 +0200

    Project setup

---
+++ playwright.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 60_000,
+  expect: { timeout: 10_000 },
+  fullyParallel: true,
+  retries: 0,
+  workers: undefined,
+  reporter: 'html',
+  use: {
+    actionTimeout: 0,
+    trace: 'retain-on-failure',
+    video: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
